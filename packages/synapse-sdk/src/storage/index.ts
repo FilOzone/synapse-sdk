4c0cc4721500e3a857982cb5fb0c63c2f1834413
Hugo Dias <hugomrdias@gmail.com>
2025-12-12 12:35:04 +0000

    feat!: change to only export Synapse from the main entrypoint
    closes #232

--- packages/synapse-sdk/src/storage/index.ts
+++ packages/synapse-sdk/src/storage/index.ts
@@ -1,14 +1,12 @@
 /**
- * Exports the Storage components
+ * Storage components
  *
- * @packageDocumentation
  * @module Storage
  * @example
  * ```ts
- * import { StorageContext, StorageManager, StorageService } from '@filoz/synapse-sdk/storage'
+ * import { StorageContext, StorageManager } from '@filoz/synapse-sdk/storage'
  * ```
  */
 
 export { StorageContext } from './context.ts'
 export { StorageManager } from './manager.ts'
-export { StorageService } from './service.ts'
