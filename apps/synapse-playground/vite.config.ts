6bc161989f6feaa6f6c13d055836328688a121fd
Hugo Dias <hugomrdias@gmail.com>
2025-12-16 15:04:32 +0000

    chore: add plausible to docs (#512)

--- apps/synapse-playground/vite.config.ts
+++ apps/synapse-playground/vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
-import { analyzer } from 'vite-bundle-analyzer'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
@@ -16,9 +15,6 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
-    analyzer({
-      enabled: false,
-    }),
   ],
   resolve: {
     dedupe: ['react', 'react-dom', 'wagmi'],
