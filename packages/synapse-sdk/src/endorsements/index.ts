8020db0a4e3086d6c2bf32c1be50a5beb1634f52
William Morriss <wjmelements@gmail.com>
2026-01-13 16:34:42 -0600

    EndorsementsService

---
+++ packages/synapse-sdk/src/endorsements/index.ts
@@ -0,0 +1,1 @@
+export { EndorsementsService } from './service.ts'
---
+++ packages/synapse-sdk/src/endorsements/service.ts
@@ -0,0 +1,29 @@
+import { ethers } from 'ethers'
+import { CONTRACT_ABIS } from '../utils/index.ts'
+
+/**
+ * Endorsed storage providers have a strong durability record and are held to higher standards.
+ * A ProviderIdSet smart contract governs the membership of this group.
+ */
+export class EndorsementsService {
+  private readonly _endorsementsContract: ethers.Contract
+  private _endorsedProviderIds: number[] | null = null
+
+  private constructor(provider: ethers.Provider, endorsementsAddress: string) {
+    this._endorsementsContract = new ethers.Contract(endorsementsAddress, CONTRACT_ABIS.ENDORSEMENTS, provider)
+  }
+
+  /**
+   *
+   * @returns Array of endorsed storage provider ids
+   */
+  async getEndorsedProviderIds(): Promise<number[]> {
+    if (this._endorsedProviderIds == null) {
+      const endorsedProviderIds = await this._endorsementsContract.getProviderIds()
+      this._endorsedProviderIds = endorsedProviderIds
+      return endorsedProviderIds
+    } else {
+      return this._endorsedProviderIds
+    }
+  }
+}
