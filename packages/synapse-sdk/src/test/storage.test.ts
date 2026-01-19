7f757c78ffa1fce01d669a4d6c14208019ee37e4
Miroslav Bajtoš <oss@bajtos.net>
2026-01-19 12:19:35 +0100

    fixup! fix the failing test
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- packages/synapse-sdk/src/test/storage.test.ts
+++ packages/synapse-sdk/src/test/storage.test.ts
@@ -1535,7 +1535,7 @@ describe('StorageService', () => {
               maxPieceSizeInBytes: 34359738368n,
               ipniPiece: false,
               ipniIpfs: false,
-              ipniPeerID: '',
+              ipniPeerID: undefined,
               storagePricePerTibPerDay: 1000000n,
               minProvingPeriodInEpochs: 30n,
               location: 'us-east',
