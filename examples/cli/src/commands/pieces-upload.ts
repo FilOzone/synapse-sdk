1ef8d97b0c48f3a5ff76408283a2e48c53b118fd
Hugo Dias <hugomrdias@gmail.com>
2026-02-06 19:06:17 +0000

    chore: update cli example

--- examples/cli/src/commands/pieces-upload.ts
+++ examples/cli/src/commands/pieces-upload.ts
@@ -1,8 +1,7 @@
 import path from 'node:path'
 import * as p from '@clack/prompts'
 import * as SP from '@filoz/synapse-core/sp'
-import { upload } from '@filoz/synapse-core/warm-storage'
-import { openFile } from '@remix-run/fs'
+import { openLazyFile } from '@remix-run/fs'
 import { type Command, command } from 'cleye'
 import { privateKeyClient } from '../client.ts'
 import { globalFlags } from '../flags.ts'
@@ -27,29 +26,28 @@ export const piecesUpload: Command = command(
   },
   async (argv) => {
     const { client, chain } = privateKeyClient(argv.flags.chain)
-    const spinner = p.spinner()
+    // const spinner = p.spinner()
 
     const filePath = argv._.path
     const absolutePath = path.resolve(filePath)
-    const file = openFile(absolutePath)
+    const file = openLazyFile(absolutePath)
 
-    spinner.start(`Uploading file ${absolutePath}...`)
+    p.log.info(`Uploading file ${absolutePath}...`)
     try {
-      const result = await upload(client, {
+      const result = await SP.upload(client, {
         dataSetId: BigInt(argv._.dataSetId),
         data: [file],
         onEvent: (event, data) => {
-          spinner.message(`${event} ${data.pieceCid.toString()}`)
+          p.log.info(`${event} ${data.pieceCid.toString()}`)
         },
       })
 
-      spinner.message(
+      p.log.info(
         `Waiting for tx ${hashLink(result.txHash, chain)} to be mined...`
       )
-      const pieces = await SP.waitForAddPiecesStatus(result)
-      spinner.stop(`File uploaded ${pieces.confirmedPieceIds.join(',')}`)
+      const pieces = await SP.waitForAddPieces(result)
+      p.log.info(`File uploaded ${pieces.confirmedPieceIds.join(',')}`)
     } catch (error) {
-      spinner.stop()
       p.log.error((error as Error).message)
       p.outro('Please try again')
       return
--- examples/cli/src/utils.ts
+++ examples/cli/src/utils.ts
@@ -1,5 +1,13 @@
+import * as p from '@clack/prompts'
 import type { Chain } from '@filoz/synapse-core/chains'
+import { getPieces } from '@filoz/synapse-core/pdp-verifier'
+import { getPDPProviders } from '@filoz/synapse-core/sp-registry'
+import {
+  getPdpDataSets,
+  type PdpDataSet,
+} from '@filoz/synapse-core/warm-storage'
 import terminalLink from 'terminal-link'
+import type { Account, Client, Transport } from 'viem'
 
 export function hashLink(hash: string, chain: Chain) {
   const link = terminalLink(
@@ -7,4 +15,131 @@ export function hashLink(hash: string, chain: Chain) {
     `${chain.blockExplorers?.default?.url}/tx/${hash}`
   )
   return link
+}
+
+export async function selectDataSet(
+  client: Client<Transport, Chain, Account>,
+  options: { debug?: boolean }
+) {
+  const spinner = p.spinner()
+  spinner.start(`Fetching data sets...`)
+
+  try {
+    const dataSets = await getPdpDataSets(client, {
+      client: client.account.address,
+    })
+    spinner.stop(`Data sets fetched.`)
+
+    if (dataSets.length === 0) {
+      p.cancel('No data sets found.')
+      process.exit(1)
+    }
+
+    const dataSetId = await p.select({
+      message: 'Select a data set:',
+      options: dataSets.map((dataSet) => ({
+        value: dataSet.dataSetId,
+        label: `#${dataSet.dataSetId} - SP: #${dataSet.providerId} ${dataSet.provider.pdp.serviceURL} ${dataSet.pdpEndEpoch > 0n ? `Terminating at epoch ${dataSet.pdpEndEpoch}` : ''}`,
+      })),
+    })
+    if (p.isCancel(dataSetId)) {
+      p.cancel('Operation cancelled.')
+      process.exit(1)
+    }
+
+    return dataSetId
+  } catch (error) {
+    spinner.error('Failed to select data set')
+    if (options.debug) {
+      console.error(error)
+    } else {
+      p.log.error((error as Error).message)
+    }
+    process.exit(1)
+  }
+}
+
+export async function selectPiece(
+  client: Client<Transport, Chain, Account>,
+  dataSet: PdpDataSet,
+  options: { debug?: boolean }
+) {
+  const spinner = p.spinner()
+  spinner.start(`Fetching pieces...`)
+
+  try {
+    const pieces = await getPieces(client, {
+      dataSet,
+      address: client.account.address,
+    })
+    spinner.stop(`Pieces fetched.`)
+
+    if (pieces.pieces.length === 0) {
+      p.cancel('No pieces found.')
+      process.exit(1)
+    }
+
+    const pieceId = await p.select({
+      message: 'Select a piece:',
+      options: pieces.pieces.map((piece) => ({
+        value: piece.id,
+        label: `#${piece.id} ${piece.cid}`,
+      })),
+    })
+    if (p.isCancel(pieceId)) {
+      p.cancel('Operation cancelled.')
+      process.exit(1)
+    }
+
+    return pieceId
+  } catch (error) {
+    spinner.error('Failed to select piece')
+    if (options.debug) {
+      console.error(error)
+    } else {
+      p.log.error((error as Error).message)
+    }
+    process.exit(1)
+  }
+}
+
+export async function selectProvider(
+  client: Client<Transport, Chain, Account>,
+  options: { debug?: boolean }
+) {
+  const spinner = p.spinner()
+  spinner.start(`Fetching providers...`)
+
+  try {
+    const { providers } = await getPDPProviders(client)
+    spinner.stop(`Providers fetched.`)
+
+    if (providers.length === 0) {
+      p.cancel('No providers found.')
+      process.exit(1)
+    }
+
+    const providerId = await p.select({
+      message: 'Select a provider:',
+
+      options: providers.map((provider) => ({
+        value: provider.id,
+        label: `#${provider.id} - ${provider.serviceProvider} ${provider.pdp.serviceURL}`,
+      })),
+    })
+    if (p.isCancel(providerId)) {
+      p.cancel('Operation cancelled.')
+      process.exit(1)
+    }
+
+    return providers.find((provider) => provider.id === providerId)
+  } catch (error) {
+    spinner.error('Failed to select provider')
+    if (options.debug) {
+      console.error(error)
+    } else {
+      p.log.error((error as Error).message)
+    }
+    process.exit(1)
+  }
 }
